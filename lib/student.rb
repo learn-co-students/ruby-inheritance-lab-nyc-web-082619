fe79f0c915b0cee5c2d3d2c3c1c891cccc087864
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-03 16:34:55 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative 'User'
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge.push(knowledge)
+    end
+
 
 end
